2cde723d352401d9605bd14ca10800c48ca2e19c
soro kiyali <sorokiyali21@gmail.com>
2023-11-03 15:57:08 +0000

    le work du 03112023

---
+++ index.js
@@ -0,0 +1,15 @@
+// function surfaceREct(L, l){
+//     let result = L*L;
+//     return result;
+// }
+// console.log(surfaceREct(4,3));
+
+// const surfaceREct = (L,l) => {
+//     let result = L*L;
+//     return result;
+// }
+// console.log(surfaceREct(4,3));
+// const surfaceREct = (L,l) =>  L*l;
+// console.log(surfaceREct(20,3));
+const salutation = nom => console.log('salut',nom);
+(salutation('Soro'));
